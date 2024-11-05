66d678e6a61f99591e2cdf7e5cb99b12c176cf1b
Abraham HC <hernandez.c.abraham.00@gmail.com>
2024-11-05 17:47:26 -0600

    Membership Module created

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { PaymentModule } from './payment/payment.module';
 import { ProductModule } from './product/product.module';
 import { SeedModule } from './seed/seed.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { MembershipModule } from './membership/membership.module';
 
 @Module({
-  imports: [AuthModule, CommonModule, OrdersModule, PaymentModule, ProductModule, SeedModule, PrismaModule],
+  imports: [AuthModule, CommonModule, OrdersModule, PaymentModule, ProductModule, SeedModule, PrismaModule, MembershipModule],
   controllers: [],
   providers: [],
 })
