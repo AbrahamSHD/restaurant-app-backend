d303ec50adbe7376944909cc0c519661e4d24547
Abraham HC <hernandez.c.abraham.00@gmail.com>
2024-11-05 17:22:48 -0600

    Envs updated

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,11 +3,21 @@ import * as joi from 'joi';
 
 interface EnvVars {
   PORT: number;
+  DB_NAME: string;
+  POSTGRES_USER: string;
+  POSTGRES_PASSWORD: string;
+  DATABASE_URL: string;
+  JWT_SECRET: string;
 }
 
 const envsSchema = joi
   .object({
     PORT: joi.number().required(),
+    DB_NAME: joi.string().required(),
+    POSTGRES_USER: joi.string().required(),
+    POSTGRES_PASSWORD: joi.string().required(),
+    DATABASE_URL: joi.string().required(),
+    JWT_SECRET: joi.string().required(),
   })
   .unknown(true);
 
@@ -21,4 +31,9 @@ const envVars: EnvVars = value;
 
 export const envs = {
   port: envVars.PORT,
+  dbName: envVars.DB_NAME,
+  postgresUser: envVars.POSTGRES_USER,
+  postgresPassword: envVars.POSTGRES_PASSWORD,
+  databaseUrl: envVars.DATABASE_URL,
+  jwtSecret: envVars.JWT_SECRET,
 };
