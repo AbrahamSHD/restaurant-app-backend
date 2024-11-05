faf19f24f130edbba000f5ebcf9def1e0a95bd0e
Abraham HC <hernandez.c.abraham.00@gmail.com>
2024-11-05 17:43:15 -0600

    Payment Module created

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { CommonModule } from './common/common.module';
 import { OrdersModule } from './orders/orders.module';
+import { PaymentModule } from './payment/payment.module';
 
 @Module({
-  imports: [AuthModule, CommonModule, OrdersModule],
+  imports: [AuthModule, CommonModule, OrdersModule, PaymentModule],
   controllers: [],
   providers: [],
 })
