6a77ac9a4dc12c279a9aadbe4f4bb5833ec8e850
Abraham HC <hernandez.c.abraham.00@gmail.com>
2024-11-05 17:40:26 -0600

    Orders Module created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { CommonModule } from './common/common.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [AuthModule, CommonModule],
+  imports: [AuthModule, CommonModule, OrdersModule],
   controllers: [],
   providers: [],
 })
