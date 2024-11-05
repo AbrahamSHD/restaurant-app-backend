d73351571d17dfca1d7b0e7bdf75ac03e9ca5651
Abraham HC <hernandez.c.abraham.00@gmail.com>
2024-11-05 17:44:14 -0600

    Product Module created

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AuthModule } from './auth/auth.module';
 import { CommonModule } from './common/common.module';
 import { OrdersModule } from './orders/orders.module';
 import { PaymentModule } from './payment/payment.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [AuthModule, CommonModule, OrdersModule, PaymentModule],
+  imports: [AuthModule, CommonModule, OrdersModule, PaymentModule, ProductModule],
   controllers: [],
   providers: [],
 })
