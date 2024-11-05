0b5f8350fd31937c05621fe0e4f09bfde4ea45b9
Abraham HC <hernandez.c.abraham.00@gmail.com>
2024-11-05 17:38:18 -0600

    Auth Module created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, CommonModule],
   controllers: [],
   providers: [],
 })
