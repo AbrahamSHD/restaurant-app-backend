3390c240a2a5ea24c0a285d7faf3d0144bdfe80e
Abraham HC <hernandez.c.abraham.00@gmail.com>
2024-11-05 16:41:27 -0600

    Envs configured

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { envs } from './config/envs';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger('Main');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  logger.log(`App running on port ${envs.port}`);
+  await app.listen(envs.port);
 }
 bootstrap();
