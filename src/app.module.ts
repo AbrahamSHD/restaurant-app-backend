fed65d3b0df52502a15ef50da757f95d8ec6f6da
Abraham HC <hernandez.c.abraham.00@gmail.com>
2024-11-13 19:06:25 -0600

    CommonModule.ts deleted

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
-import { CommonModule } from './common/common.module';
 import { OrdersModule } from './orders/orders.module';
 import { PaymentModule } from './payment/payment.module';
 import { ProductModule } from './product/product.module';
@@ -18,7 +17,6 @@ import { ReservationsModule } from './reservations/reservations.module';
 @Module({
   imports: [
     AuthModule,
-    CommonModule,
     OrdersModule,
     PaymentModule,
     ProductModule,
