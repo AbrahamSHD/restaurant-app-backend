b0d82163f22260a51505a965fe78fe7172fa3ddc
Abraham HC <hernandez.c.abraham.00@gmail.com>
2024-11-14 22:31:11 -0600

    AuthService updated

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,18 +1,33 @@
 import { BadRequestException, Injectable, Logger } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { CreateAuthDto, LoginAuthDto, UpdateAuthDto } from './dto';
 import { PrismaService } from '../prisma/prisma.service';
 import { bcryptAdapter, envs } from '../common/config';
-import { ExceptionHandler } from 'src/common/helpers';
+import { ExceptionHandler } from '../common/helpers';
+import { JwtPayload } from '../common/interfaces';
 
 @Injectable()
 export class AuthService {
   private readonly logger = new Logger('AuthService');
   private readonly bcrypt = new bcryptAdapter();
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
 
   defaultRoleId = envs.defaultRoleId;
 
-  async findOneByTerm(term: string) {
+  private getJwtToken(payload: JwtPayload) {
+    try {
+      const token = this.jwtService.sign(payload);
+      return token;
+    } catch (error) {
+      this.logger.error('Error generating JWT: ', error);
+      throw new BadRequestException('Could not generate JWT');
+    }
+  }
+
+  async findOneUserByTerm(term: string) {
     const user = this.prisma.users.findFirst({
       where: {
         OR: [{ id: term }, { name: term }, { email: term }],
@@ -27,7 +42,7 @@ export class AuthService {
       const { name, email, password } = createAuthDto;
       let { roleId } = createAuthDto;
 
-      const userExist = await this.findOneByTerm(email);
+      const userExist = await this.findOneUserByTerm(email);
 
       if (!!userExist) {
         throw new BadRequestException(`User with email ${email} already exist`);
@@ -59,22 +74,156 @@ export class AuthService {
   }
 
   async loginUser(loginAuthDto: LoginAuthDto) {
-    return loginAuthDto;
+    try {
+      const { id, name, email, password } = loginAuthDto;
+
+      const user = await this.findOneUserByTerm(email);
+
+      if (!user) {
+        throw new BadRequestException(`User with email not found`);
+      }
+
+      const isPasswordValid = this.bcrypt.compare(password, user.password);
+
+      if (!isPasswordValid) {
+        throw new BadRequestException('User/Password not valid');
+      }
+
+      const token = this.getJwtToken({ id });
+
+      return {
+        token,
+        user: {
+          id: user.id,
+          name,
+          email,
+        },
+      };
+    } catch (error) {
+      ExceptionHandler.handle(error);
+    }
   }
 
   async findAllUsers() {
-    return `This action returns all auth`;
+    try {
+      const users = await this.prisma.users.findMany({
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          roleId: true,
+          membership: true,
+        },
+      });
+
+      return { users };
+    } catch (error) {
+      ExceptionHandler.handle(error);
+    }
   }
 
-  async findOneUser(id: number) {
-    return `This action returns a #${id} auth`;
+  async findOneUser(id: string) {
+    try {
+      const user = await this.findOneUserByTerm(id);
+
+      if (!user) {
+        throw new BadRequestException(`User with id: ${id} not fund`);
+      }
+
+      delete user.password;
+
+      return { user };
+    } catch (error) {
+      ExceptionHandler.handle(error);
+    }
   }
 
+  // async updateUser(id: string, updateAuthDto: UpdateAuthDto) {
+  //   try {
+  //     const userExist = await this.findOneUserByTerm(id);
+
+  //     if (!userExist) {
+  //       throw new BadRequestException(`User with id: ${id} not fund`);
+  //     }
+
+  //     const userUpdated = this.prisma.users.update({
+  //       where: { id },
+  //       data: {
+  //         ...updateAuthDto,
+  //       },
+  //     });
+
+  //     return { user: { userUpdated } };
+  //   } catch (error) {
+  //     ExceptionHandler.handle(error);
+  //   }
+  // }
   async updateUser(id: string, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth: ${updateAuthDto}`;
+    try {
+      const userExist = await this.findOneUserByTerm(id);
+
+      if (!userExist) {
+        throw new BadRequestException(`User with id: ${id} not found`);
+      }
+
+      // TODO: Validar el cambio de email
+
+      const { roleId, membershipId, reservations, orders, ...otherFields } =
+        updateAuthDto;
+
+      if (!!reservations) {
+        throw new BadRequestException(
+          `Please update your reservations in the reservation module.`,
+        );
+      }
+
+      if (!!orders) {
+        throw new BadRequestException(
+          `Please update your reservations in the reservation module.`,
+        );
+      }
+
+      if (!!orders) {
+        throw new BadRequestException(`Membership Module under construction`);
+      }
+
+      const userUpdated = await this.prisma.users.update({
+        where: { id },
+        data: {
+          ...otherFields,
+          ...(roleId ? { role: { connect: { id: roleId } } } : {}),
+          ...(membershipId
+            ? { membership: { connect: { id: membershipId } } }
+            : {}),
+        },
+      });
+
+      delete userUpdated.password;
+
+      return { user: userUpdated };
+    } catch (error) {
+      ExceptionHandler.handle(error);
+    }
   }
 
   async removeUser(id: string) {
-    return `This action removes a #${id} auth`;
+    try {
+      const userExist = await this.findOneUserByTerm(id);
+
+      if (!userExist) {
+        throw new BadRequestException(`User with id: ${id} not found`);
+      }
+
+      await this.prisma.users.delete({
+        where: { id },
+      });
+
+      return {
+        status: 200,
+        message: 'ok',
+      };
+    } catch (error) {
+      ExceptionHandler.handle(error);
+    }
   }
 }
