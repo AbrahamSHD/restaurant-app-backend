4428dc2cde22c7970109ce43aa08d028b1bc285f
Abraham HC <hernandez.c.abraham.00@gmail.com>
2024-11-14 19:16:03 -0600

    Auth service - endpoint registerUser created

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,29 +1,80 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, Logger } from '@nestjs/common';
 import { CreateAuthDto, LoginAuthDto, UpdateAuthDto } from './dto';
+import { PrismaService } from '../prisma/prisma.service';
+import { bcryptAdapter, envs } from '../common/config';
+import { ExceptionHandler } from 'src/common/helpers';
 
 @Injectable()
 export class AuthService {
-  registerUser(createAuthDto: CreateAuthDto) {
-    return createAuthDto;
+  private readonly logger = new Logger('AuthService');
+  private readonly bcrypt = new bcryptAdapter();
+  constructor(private prisma: PrismaService) {}
+
+  defaultRoleId = envs.defaultRoleId;
+
+  async findOneByTerm(term: string) {
+    const user = this.prisma.users.findFirst({
+      where: {
+        OR: [{ id: term }, { name: term }, { email: term }],
+      },
+    });
+
+    return user;
   }
 
-  loginUser(loginAuthDto: LoginAuthDto) {
+  async registerUser(createAuthDto: CreateAuthDto) {
+    try {
+      const { name, email, password } = createAuthDto;
+      let { roleId } = createAuthDto;
+
+      const userExist = await this.findOneByTerm(email);
+
+      if (!!userExist) {
+        throw new BadRequestException(`User with email ${email} already exist`);
+      }
+
+      if (!roleId) {
+        roleId = this.defaultRoleId;
+      }
+
+      const encryptedPassword = this.bcrypt.hash(password);
+
+      const newUser = await this.prisma.users.create({
+        data: {
+          name,
+          email,
+          password: encryptedPassword,
+          roleId,
+        },
+      });
+
+      delete newUser.password;
+
+      return {
+        user: newUser,
+      };
+    } catch (error) {
+      ExceptionHandler.handle(error);
+    }
+  }
+
+  async loginUser(loginAuthDto: LoginAuthDto) {
     return loginAuthDto;
   }
 
-  findAllUsers() {
+  async findAllUsers() {
     return `This action returns all auth`;
   }
 
-  findOneUser(id: number) {
+  async findOneUser(id: number) {
     return `This action returns a #${id} auth`;
   }
 
-  updateUser(id: string, updateAuthDto: UpdateAuthDto) {
+  async updateUser(id: string, updateAuthDto: UpdateAuthDto) {
     return `This action updates a #${id} auth: ${updateAuthDto}`;
   }
 
-  removeUser(id: string) {
+  async removeUser(id: string) {
     return `This action removes a #${id} auth`;
   }
 }
