c5a1f557557da07e6f92ffdf5d8e6ea382cce091
Abraham HC <hernandez.c.abraham.00@gmail.com>
2024-11-21 14:23:49 -0600

    Order.dto created

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,1 +1,37 @@
-export class CreateOrderDto {}
+import {
+  IsDate,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  IsUUID,
+} from 'class-validator';
+
+export class CreateOrderDto {
+  @IsOptional()
+  @IsString()
+  @IsUUID()
+  id: string;
+
+  @IsOptional()
+  @IsString()
+  @IsUUID()
+  userId: string;
+
+  @IsOptional()
+  @IsDate()
+  createdAt: string;
+
+  @IsNotEmpty()
+  @IsString()
+  status: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  totalPrice: number;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsUUID()
+  tableId: string;
+}
